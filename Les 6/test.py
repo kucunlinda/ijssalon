86f1c905040f2fd2d0f354d1fc0aa005139c879c
Linda <kucunlinda@gmail.com>
2025-10-23 12:59:07 +0200

    les6

---
+++ Les 6/test.py
@@ -0,0 +1,16 @@
+mijn_lijst = ["waffels", "softijd", "schepijs", "pannekoeken"]
+#for item in mijn_lijst:
+#    print(f"wij verkopen {item}")
+#mijn_lijst.append("muffins")
+#for item in mijn_lijst:
+#    print(f"wij verkopen", item)
+#alfabet = ["a", "b", "d", "e", "f"]
+#print(alfabet)
+#alfabet.insert(2, "c")
+#print(alfabet)
+#fruit = ["appel", "banaan", "kers"]
+#fruit.pop(1)
+#print(fruit)
+fruit = ["druif", "sinaas", "perzik", "kiwi'"]
+fruit.remove("perzik")
+print(fruit)
