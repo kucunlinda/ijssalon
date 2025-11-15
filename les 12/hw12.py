f21176a6b9a8b4ff3079ec6397ada66b8b2430dc
Linda <kucunlinda@gmail.com>
2025-11-15 14:35:00 +0100

    huiswerk les 12

---
+++ les 12/hw12.py
@@ -0,0 +1,10 @@
+def vergelijk_getallen():
+    getal1 = int(input("Wat is getal 1? "))
+    getal2 = int(input("Wat is getal 2? "))
+    if getal1 == getal2:
+        return True
+    else:
+        return False
+print(vergelijk_getallen())
+
+
